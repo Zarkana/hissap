527255f9b14e001114375de047399b766f543f97
Zarkana <jsc940@gmail.com>
2016-06-01 16:26:51 -1000

    Added styling to details page

--- HISSAP1/Models/Organization.cs
+++ HISSAP1/Models/Organization.cs
@@ -9,15 +9,30 @@ namespace HISSAP1.Models
   public class Organization
   {
     public int ID { get; set; }
+    [Required]
     public string Name { get; set; }
+    [Required]
+    [Display(Name = "Address 1st Line")]
     public string Line1 { get; set; }
+    [Display(Name = "Address 2nd Line")]
     public string Line2 { get; set; }
+    [Required]
     public string City { get; set; }
+    [Required]
     public string State { get; set; }
+    [Required]
+    [RegularExpression(@"^\d{5}(-\d{4})?", ErrorMessage = "Please enter a valid US Zip")]
     public string Zip { get; set; }
+    [Display(Name = "Contact Name")]
     public string ContactPerson { get; set; }
-    public string Phone { get; set; }
-    public string Website { get; set; }
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+    [Required]
+    [Phone]    
+    [RegularExpression(@"[\(]\d{3}[\)]-?\d{3}[\-]\d{4}", ErrorMessage= "Format: (999)-999-9999")]
+    public string Phone { get; set; }
+    [Url]
+    public string Website { get; set; }
   }
 }
