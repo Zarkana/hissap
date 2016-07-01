0579071a50a084bba5038f45009dcde3a93fcb57
Joseph Campbell <jsc940@gmail.com>
2016-06-30 16:40:38 -1000

    Stable version that has select site dropdown not breaking everything

---
+++ HISSAP1/Migrations/201607010106322_adding viewmodel stuff.cs
@@ -0,0 +1,16 @@
+namespace HISSAP1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addingviewmodelstuff : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
