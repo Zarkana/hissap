b2a3f3e4693ad07caf1f4ea231a54d6ffc93cf4d
Joseph Campbell <jsc940@gmail.com>
2016-07-15 15:58:32 -1000

    Stable version of budget

--- HISSAP1/Controllers/PartialsController.cs
+++ HISSAP1/Controllers/PartialsController.cs
@@ -11,50 +11,51 @@ using System.Web.Mvc;
 
 namespace HISSAP1.Controllers
 {
-  [ChildActionOnly]
-  public class PartialsController : MyBaseController
-  {
-    private ApplicationDbContext db = new ApplicationDbContext();
-
-    public ActionResult ChangeSitePartial()
+    [ChildActionOnly]
+    public class PartialsController : MyBaseController
     {
-      
-      string id = User.Identity.GetUserId();//Get user id
-      if (id == null) { return new HttpStatusCodeResult(HttpStatusCode.BadRequest); }//check if exists
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-      //Get the current users currentSite
-      CurrentSite currentSite = db.CurrentSite.Find(id);
+        public ActionResult ChangeSitePartial()
+        {
 
-      if (currentSite == null) { return HttpNotFound(); }//check if exists
+            string id = User.Identity.GetUserId();//Get user id
+            if (id == null) { return new HttpStatusCodeResult(HttpStatusCode.BadRequest); }//check if exists
 
-      //TODO: Change provider name to ProviderName, for consitency with site and contract
-      ViewBag.SelectedProvider = new SelectList(db.Providers, "Id", "Name", currentSite.Site.SitesContract.ContractsProviderId);
-      ViewBag.SelectedContract = new SelectList(db.Contracts, "Id", "ContractName", currentSite.Site.SitesContractId);
-      ViewBag.SelectedSite = new SelectList(db.Sites, "Id", "SiteName", currentSite.SelectedSite);
-      ViewBag.UserId = new SelectList(db.Users, "Id", "Email", currentSite.UserId);
+            //Get the current users currentSite
+            CurrentSite currentSite = db.CurrentSite.Find(id);
 
-      return PartialView("_ChangeSitePartial", currentSite);
+            if (currentSite == null) { return HttpNotFound(); }//check if exists
 
+            //TODO: Change provider name to ProviderName, for consitency with site and contract
+            ViewBag.SelectedProvider = new SelectList(db.Providers, "Id", "Name", currentSite.Site.SitesContract.ContractsProviderId);
+            ViewBag.SelectedContract = new SelectList(db.Contracts, "Id", "ContractName", currentSite.Site.SitesContractId);
+            ViewBag.SelectedSite = new SelectList(db.Sites, "Id", "SiteName", currentSite.SelectedSite);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", currentSite.UserId);
+
+            return PartialView("_ChangeSitePartial", currentSite);
+
+        }
+
+        //Breaking up into separate pages
+
+        //public ActionResult PayrollTaxesAssessmentPartial(PayrollTaxesAssessment model)
+        //{
+        //  return PartialView("Budgets/PayrollTaxesAssessment", model);
+        //}
+
+        public ActionResult PayrollItemsPartial(/*int id*/)
+        {
+            //PayrollItem model = new PayrollItem();
+            //model.PayrollTaxesAssessmentId = id;
+            return PartialView("Budgets/PayrollItem");
+        }
+
+        //public ActionResult FringeBenefitsPartial()
+        //{
+        //  return PartialView("Budgets/FringeBenefits");
+        //}
     }
-
-    //Breaking up into separate pages
-
-    //public ActionResult PayrollTaxesAssessmentPartial(PayrollTaxesAssessment model)
-    //{
-    //  return PartialView("Budgets/PayrollTaxesAssessment", model);
-    //}
-
-    public ActionResult PayrollItemsPartial(PayrollItem model)
-    {
-      ViewBag.PayrollTaxesAssessmentId = new SelectList(db.PayrollTaxesAssessments, "Id", "Id");
-      return PartialView("Budgets/PayrollItem", model);
-    }
-
-    //public ActionResult FringeBenefitsPartial()
-    //{
-    //  return PartialView("Budgets/FringeBenefits");
-    //}
-  }
 
 
 }
